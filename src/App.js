4191804b3d230ab43d79be3ba20d4ed6c47d803c
Alexander-webov <vilmenshtain@yandex.ru>
2021-11-11 18:59:10 +0300

    start project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Create from './componets/Create';
+import Main from './componets/Main';
+import About from './componets/About';
+import Footer from './componets/Footer';
+import Note from './componets/Note';
+import Error from './componets/Error';
+import Header from './componets/Header';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <Router>
+        <Header></Header>
+        <Switch>
+          {/*  <Route exact path="/" component={Main} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/note/" component={Note} />
+          <Route exact path="/note/:noteURL" component={Note} />
+          <Route exact path="/error" component={Error} />
+          <Route exact path="/create" component={Create} /> */}
+
+        </Switch>
+      </Router>
+      <Footer />
     </div>
   );
 }
